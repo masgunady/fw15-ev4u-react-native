489aca07b6e43f3032f07cd136a0232910d2e47d
Gunadi PS <gndsulistiawan@gmail.com>
2023-06-25 00:55:24 +0700

    feat: splash screen slicing page

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {SplashScreen} from './src/screens';
+
+const App = () => {
+  return <SplashScreen />;
+};
+
+export default App;
